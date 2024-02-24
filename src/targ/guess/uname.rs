fb101710b7e09b79a2cd1ca62494204b4342b181
Connor Horman <chorman64@gmail.com>
2024-02-24 14:26:35 -0500

    feat(autobuild): Support windows in `cargo autobuild guess`

--- src/targ/guess/uname.rs
+++ src/targ/guess/uname.rs
@@ -8,42 +8,95 @@ pub struct Uname {
 }
 
 pub fn uname() -> io::Result<Uname> {
-    let kernel = std::process::Command::new("uname").arg("-s").output()?;
-    let arch = std::process::Command::new("uname").arg("-m").output()?;
-    let sys = std::process::Command::new("uname").arg("-o").output()?;
+    cfg_match::cfg_match! {
+        unix => ({
+            let kernel = std::process::Command::new("uname").arg("-s").output()?;
+            let arch = std::process::Command::new("uname").arg("-m").output()?;
+            let sys = std::process::Command::new("uname").arg("-o").output()?;
 
-    if !kernel.status.success() {
-        return Err(io::Error::new(
-            io::ErrorKind::NotFound,
-            format!("Could not determine target system name (executing `uname -s` failed)"),
-        ));
+            if !kernel.status.success() {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    format!("Could not determine target system name (executing `uname -s` failed)"),
+                ));
+            }
+
+            let kernel = core::str::from_utf8(&kernel.stdout)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+                .trim()
+                .to_string();
+
+            if !arch.status.success() {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    format!("Could not determine target system name (executing `uname -k` failed)"),
+                ));
+            }
+            let arch = core::str::from_utf8(&arch.stdout)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+                .trim()
+                .to_string();
+
+            let sys = if sys.status.success() {
+                let sys = core::str::from_utf8(&sys.stdout)
+                    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+                    .trim()
+                    .to_string();
+                Some(sys)
+            } else {
+                None
+            };
+
+            Ok(Uname { kernel, arch, sys })
+        }),
+        windows => ({
+            let kernel = std::process::Command::new("uname").arg("-s").output();
+
+            let kernel = match kernel{
+                Ok(kernel) => kernel,
+                Err(_) => {
+                    return Ok(Uname{kernel: format!("Windows NT"), arch: std::env::var("PROCESSOR_ARCHITECTURE").map_err(|e| io::Error::new(io::ErrorKind::NotFound, e)), sys: None})
+                }
+            }
+
+            let arch = std::process::Command::new("uname").arg("-m").output()?;
+            let sys = std::process::Command::new("uname").arg("-o").output()?;
+
+            if !kernel.status.success() {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    format!("Could not determine target system name (executing `uname -s` failed)"),
+                ));
+            }
+
+            let kernel = core::str::from_utf8(&kernel.stdout)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+                .trim()
+                .to_string();
+
+            if !arch.status.success() {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    format!("Could not determine target system name (executing `uname -k` failed)"),
+                ));
+            }
+            let arch = core::str::from_utf8(&arch.stdout)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+                .trim()
+                .to_string();
+
+            let sys = if sys.status.success() {
+                let sys = core::str::from_utf8(&sys.stdout)
+                    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+                    .trim()
+                    .to_string();
+                Some(sys)
+            } else {
+                None
+            };
+
+            Ok(Uname { kernel, arch, sys })
+        })
+        _ => panic!("We don't know how to determine target system name. This is a bug if lccc has host support on this target")
     }
-
-    let kernel = core::str::from_utf8(&kernel.stdout)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
-        .trim()
-        .to_string();
-
-    if !arch.status.success() {
-        return Err(io::Error::new(
-            io::ErrorKind::NotFound,
-            format!("Could not determine target system name (executing `uname -k` failed)"),
-        ));
-    }
-    let arch = core::str::from_utf8(&arch.stdout)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
-        .trim()
-        .to_string();
-
-    let sys = if sys.status.success() {
-        let sys = core::str::from_utf8(&sys.stdout)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
-            .trim()
-            .to_string();
-        Some(sys)
-    } else {
-        None
-    };
-
-    Ok(Uname { kernel, arch, sys })
 }
